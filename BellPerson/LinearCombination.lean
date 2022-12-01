0fb09ed61d79aeb4667eb9a91a745ee50f88e709
DanielRrr <daniel.rogozin@serokell.io>
2022-12-01 16:14:40 +0000

    stuff

--- BellPerson/LinearCombination.lean
+++ BellPerson/LinearCombination.lean
@@ -5,19 +5,24 @@ inductive Index : Type _ where
 structure Variable (A : Type _) where
   var : A
 
+instance [Add A] : HAdd A (Variable A) (Variable A) where
+  hAdd a v := Variable.mk (a + v.var)     
+
 def mkZeroVar : Variable Index :=
   Variable.mk ∘ .Input $ 0
 
 structure LinearCombination (F : Type _) where
   comb : Array (Variable Index × F)
 
--- TODO
+def LinearCombination.size (v : LinearCombination F) : Nat
+  := v.comb.size
 
 instance [OfNat F 0] : Inhabited (LinearCombination F) where
   default := LinearCombination.mk #[(mkZeroVar, 0)]
 
 instance : Add (LinearCombination F) where
-  add := sorry
+  add v₁ v₂ :=
+    LinearCombination.mk _
 
 structure Indexer (F : Type _) where
   values : Array (USize × F)
